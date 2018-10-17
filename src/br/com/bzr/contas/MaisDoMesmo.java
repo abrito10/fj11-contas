40f59a717a4b67a8f92d5f2eb012e8696089d1d5
Adilson de Brito <abrito10@gmail.com>
2018-10-17 10:48:54 -0300

    2

--- src/br/com/bzr/contas/MaisDoMesmo.java
+++ src/br/com/bzr/contas/MaisDoMesmo.java
@@ -4,6 +4,6 @@ public class MaisDoMesmo {
 	public static void main(String[] args) {
 		System.out.println("mais do mesmo");
 		System.out.println("feito pelo mac");
-		System.out.println("novo");
+		System.out.println("novosss");
 	}
 }
